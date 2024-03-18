faefdf8de5dd15a673e84db4ef256e986a6fe017
melisekm <melisekmartin1@gmail.com>
2024-03-18 14:17:04 +0100

    ambulance waiting list

--- cmd/ambulance-api-service/main.go
+++ cmd/ambulance-api-service/main.go
@@ -5,9 +5,13 @@ import (
 	"os"
 	"strings"
 
+	"context"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/melisekm/ambulance-webapi/api"
 	"github.com/melisekm/ambulance-webapi/internal/ambulance_wl"
+	"github.com/melisekm/ambulance-webapi/internal/db_service"
+	"time"
 )
 
 func main() {
@@ -22,6 +26,23 @@ func main() {
 	}
 	engine := gin.New()
 	engine.Use(gin.Recovery())
+	corsMiddleware := cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE", "PATCH"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
+		ExposeHeaders:    []string{""},
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
+	})
+	engine.Use(corsMiddleware)
+	// setup context update  middleware
+	dbService := db_service.NewMongoService[ambulance_wl.Ambulance](db_service.MongoServiceConfig{})
+	defer dbService.Disconnect(context.Background())
+	engine.Use(func(ctx *gin.Context) {
+		ctx.Set("db_service", dbService)
+		ctx.Next()
+	})
+
 	// request routings
 	ambulance_wl.AddRoutes(engine)
 	engine.GET("/openapi", api.HandleOpenApi)
